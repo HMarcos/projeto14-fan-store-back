0b8afa15a5404b9feda686e9f1d1b6b8ea1751ad
Mariana Ito <marianaitopinto@gmail.com>
2022-05-13 00:20:29 -0300

    getting product by id

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import homeRouter from './homeRouter.js';
+import productRouter from './productRouter.js';
 
 const routers = Router();
 
 routers.use(homeRouter);
+routers.use(productRouter);
 
 export default routers;
---
+++ src/routes/productRouter.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import { getProductById } from '../controllers/productController.js';
+
+const productRouter = Router();
+
+productRouter.get('/product/:productId', getProductById);
+
+export default productRouter;
