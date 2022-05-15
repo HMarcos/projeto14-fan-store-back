b2fc82eae412fef59f092f8d2d60f4d784e7165a
Marcos Henrique <marcosmarcone48@gmail.com>
2022-05-15 15:44:51 -0300

    Create tokenMiddleware.js

---
+++ src/middlewares/tokenMiddleware.js
@@ -0,0 +1,42 @@
+import db from "./../db.js";
+
+
+async function validateToken(req, res, next) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer", "").trim();
+
+    if (!token) {
+        console.log("Could not parse token...");
+        return res.status(401).send("Could not validate user, no token");
+    }
+
+    try {
+        let query = { $and: [{ token }, { status: "active" }] };
+        const session = await db.collection("sessions").findOne(query);
+
+        if (!session) {
+            console.log("Could not find a session...");
+            return res.status(401).send("Could not validate user, invalid token");
+        }
+
+        query = { _id: session.userId };
+        const user = await db.collection("users").findOne(query);
+
+        if (!user) {
+            console.log("Could not find a user...");
+            return res.status(404).send("Could not validate user, invalid user");
+        }
+
+        res.locals.user = user;
+        console.log("Token is valid...")
+        next();
+
+    } catch (e) {
+        console.log("Server Internal error... \n", e);
+        return res.sendStatus(500);
+    }
+
+
+};
+
+export default validateToken;
